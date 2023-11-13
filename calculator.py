568ffd239fefb0053692355a2b7e69e8fc806832
JP <jose@palako.com>
2023-11-13 19:20:31 +0000

    test commit fixes #10069

--- calculator.py
+++ calculator.py
@@ -21,11 +21,3 @@ class Calculator:
             return math.sqrt(alpha)
         else:
             raise ValueError('Cannot calculate the square root of a negative number.')
-
-# Example usage:
-# calc = Calculator()
-# result_addition = calc.f_add(10, 5)
-# result_subtraction = calc.f_subtract(10, 5)
-# result_multiplication = calc.f_multiply(10, 5)
-# result_division = calc.f_divide(10, 5)
-# result_square_root = calc.f_square_root(25)
