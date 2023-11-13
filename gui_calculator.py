bf08d2b79c0b8f767e8fcadbc6b8932e5c2c9618
JP <jose@palako.com>
2023-11-13 19:22:24 +0000

    test commit fixes #10069

--- gui_calculator.py
+++ gui_calculator.py
@@ -17,7 +17,8 @@ class f_GUI_Calculator:
             '7', '8', '9', '+',
             '4', '5', '6', '-',
             '1', '2', '3', '*',
-            '0', 'C', '/', '='
+            '0', 'C', '/', '=',
+            'sqrt'
         ]
 
         f_self.buttons = {}
@@ -27,7 +28,7 @@ class f_GUI_Calculator:
             f_row, f_col = divmod(f_index, 4)
             f_self.buttons[f_text].grid(row=f_row, column=f_col, sticky='nsew', padx=2, pady=2)
 
-        for f_i in range(4):
+        for f_i in range(5):
             f_self.buttons_frame.columnconfigure(f_i, weight=1)
             f_self.buttons_frame.rowconfigure(f_i, weight=1)
 
@@ -43,6 +44,8 @@ class f_GUI_Calculator:
             f_self.f_append_operator(f_value)
         elif f_value == '=':
             f_self.f_calculate_result()
+        elif f_value == 'sqrt':
+            f_self.f_calculate_square_root()
 
     def f_append_display(f_self, f_value):
         f_self.display.insert(tk.END, f_value)
@@ -57,6 +60,17 @@ class f_GUI_Calculator:
         f_expression = f_self.display.get()
         try:
             f_result = eval(f_expression, {'__builtins__': None}, {'Calculator': Calculator, 'calc': f_self.calc})
+            f_self.f_clear_display()
+            f_self.display.insert(tk.END, str(f_result))
+        except Exception as f_error:
+            f_self.f_clear_display()
+            f_self.display.insert(tk.END, 'Error')
+
+    def f_calculate_square_root(f_self):
+        f_expression = f_self.display.get()
+        try:
+            f_alpha = float(f_expression)
+            f_result = f_self.calc.f_square_root(f_alpha)
             f_self.f_clear_display()
             f_self.display.insert(tk.END, str(f_result))
         except Exception as f_error:
